e4f062e246ae226240a4596acbd0bddfd6223f53
Francesco Torchia <francesco.torchia@suse.com>
2026-02-04 11:12:34 +0100

    Add settings e2e tests
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ cypress/e2e/po/components/tabbed.po.ts
@@ -0,0 +1,15 @@
+import DefaultTabbedPo from '@rancher/cypress/e2e/po/components/tabbed.po';
+
+export default class TabbedPo extends DefaultTabbedPo {
+  constructor(selector: string) {
+    super(selector);
+  }
+
+  addTab() {
+    return this.self().get('[data-testid="tab-list-add"]').click();
+  }
+
+  removeTab() {
+    return this.self().get('[data-testid="tab-list-remove"]').click();
+  }
+}
