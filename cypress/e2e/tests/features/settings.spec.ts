a4580c527382d584f10ebc8ac713fb9b0122f410
Francesco Torchia <francesco.torchia@suse.com>
2026-02-04 11:12:34 +0100

    Add settings e2e tests
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- cypress/e2e/tests/features/settings.spec.ts
+++ cypress/e2e/tests/features/settings.spec.ts
@@ -1,21 +1,145 @@
-import { SettingsPagePo } from '@rancher/cypress/e2e/po/pages/global-settings/settings.po';
 import ProductNavPo from '@rancher/cypress/e2e/po/side-bars/product-side-nav.po';
+import { SettingsPagePo as GlobalSettings } from '@rancher/cypress/e2e/po/pages/global-settings/settings.po';
+import { SettingsPagePO } from '@/cypress/e2e/po/settings.po';
 
-describe('Settings', () => {
+describe('AI Assistant Configuration', () => {
+  const settingsPage = new SettingsPagePO();
+
+  const initValues = {
+    llm:          'ollama',
+    apiKey:       'empty',
+    fleetAgent:   'fleet',
+    rancherAgent: 'rancher',
+  };
+
+  const updatedValues = {
+    llm:         'gemini',
+    apiKey:      'my-api-key',
+    customAgent: 'agent-3'
+  };
+
   beforeEach(() => {
     cy.login();
   });
 
-  it('Should show AI Assistant settings', () => {
-    const settingsPage = new SettingsPagePo('_');
+  it('Should show settings page', () => {
+    const globalSettings = new GlobalSettings('_');
 
-    settingsPage.goTo();
-    settingsPage.waitForPage();
+    globalSettings.goTo();
+    globalSettings.waitForPage();
 
     const sideNav = new ProductNavPo();
 
     sideNav.navToSideMenuEntryByLabel('AI Assistant');
 
-    cy.contains('AI Chatbot Provider').should('be.visible');
+    cy.contains('AI Assistant Configuration').should('be.visible');
+  });
+
+  describe('AI Provider section', () => {
+    beforeEach(() => {
+      settingsPage.goTo();
+      settingsPage.waitForPage();
+    });
+
+    it('It should correctly handle the AI Assistant settings', () => {
+      const aiAgentSettings = settingsPage.settings().aiAgentSettings();
+
+      aiAgentSettings.checkExists();
+
+      aiAgentSettings.llm(initValues.llm).isSelected();
+
+      aiAgentSettings.llm(updatedValues.llm).select();
+      aiAgentSettings.llm(updatedValues.llm).isSelected();
+
+      aiAgentSettings.apiKeyInput().parent().find('.hide-show').click();
+      aiAgentSettings.apiKeyInput().should('have.value', initValues.apiKey);
+
+      aiAgentSettings.apiKeyInput().clear().type(updatedValues.apiKey);
+
+      settingsPage.settings().saveButton().click();
+
+      // Revisit the page to check if the settings were saved correctly
+      settingsPage.goTo();
+      settingsPage.waitForPage();
+
+      aiAgentSettings.llm(updatedValues.llm).isSelected();
+
+      aiAgentSettings.apiKeyInput().parent().find('.hide-show').click();
+      aiAgentSettings.apiKeyInput().should('have.value', updatedValues.apiKey);
+
+      // Revert changes
+      aiAgentSettings.apiKeyInput().clear().type(initValues.apiKey);
+      aiAgentSettings.llm(initValues.llm).select();
+
+      settingsPage.settings().saveButton().click();
+    });
+  });
+
+  describe('AI Agents section', () => {
+    beforeEach(() => {
+      settingsPage.goTo();
+      settingsPage.waitForPage();
+    });
+
+    it('It should correctly handle the AI Agent configs', () => {
+      const aiAgentConfigs = settingsPage.settings().aiAgentConfigs();
+
+      aiAgentConfigs.checkExists();
+
+      // Check that both Rancher Agent and Fleet Agent tabs are present
+      aiAgentConfigs.tabs().getTab(initValues.rancherAgent).checkExists();
+      aiAgentConfigs.tabs().getTab(initValues.fleetAgent).checkExists();
+
+      // Check that multiple agents info message is not shown when only 1 agent is enabled (Rancher Agent)
+      aiAgentConfigs.self().should('not.contain', 'Because you have multiple AI agents enabled, you will be able to choose between “Adaptive Mode”');
+
+      // Check that both Rancher Agent and Fleet Agent are enabled and locked (they are built-in agents)
+      aiAgentConfigs.tabs().assertTabIsActive(`[data-testid="${ initValues.rancherAgent }"]`);
+      aiAgentConfigs.self().should('contain', 'This AI agent is locked');
+
+      aiAgentConfigs.tabs().clickTabWithName(initValues.fleetAgent);
+      aiAgentConfigs.self().should('contain', 'This AI agent is locked');
+
+      // Add a custom AI Agent
+      aiAgentConfigs.tabs().addTab();
+
+      aiAgentConfigs.tabs().getTab(updatedValues.customAgent).checkExists();
+
+      aiAgentConfigs.tabs().assertTabIsActive(`[data-testid="${ updatedValues.customAgent }"]`);
+      aiAgentConfigs.self().should('not.contain', 'This AI agent is locked');
+
+      // We now have 2 enabled agents (Rancher Agent + custom agent), so the multiple agents info message should be shown
+      aiAgentConfigs.self().should('contain', 'Because you have multiple AI agents enabled, you will be able to choose between “Adaptive Mode”');
+
+      // Check that Save button is disabled since the new agent has validation errors -> missing MCP URL
+      settingsPage.settings().saveButton().should('be.disabled');
+
+      aiAgentConfigs.mcpUrlInput().set('http://my-mcp-url:8080');
+      settingsPage.settings().saveButton().should('be.enabled');
+
+      settingsPage.settings().saveButton().click();
+
+      // Revisit the page to check if the settings were saved correctly
+      settingsPage.goTo();
+      settingsPage.waitForPage();
+
+      aiAgentConfigs.tabs().assertTabIsActive(`[data-testid="${ updatedValues.customAgent }"]`);
+      aiAgentConfigs.mcpUrlInput().value().should('eq', 'http://my-mcp-url:8080');
+
+      // Remove the custom AI Agent
+      aiAgentConfigs.tabs().removeTab();
+
+      aiAgentConfigs.tabs().getTab(updatedValues.customAgent).checkNotExists();
+      aiAgentConfigs.self().should('not.contain', 'Because you have multiple AI agents enabled, you will be able to choose between “Adaptive Mode”');
+
+      settingsPage.settings().saveButton().click();
+
+      // Revisit the page to check if the settings were saved correctly
+      settingsPage.goTo();
+      settingsPage.waitForPage();
+
+      aiAgentConfigs.tabs().getTab(updatedValues.customAgent).checkNotExists();
+      aiAgentConfigs.tabs().assertTabIsActive(`[data-testid="${ initValues.rancherAgent }"]`);
+    });
   });
 });
