50b4035ad0eda8ee7325f56248e4df88296322b5
Francesco Torchia <francesco.torchia@suse.com>
2026-02-09 12:11:41 +0100

    Add chat name e2e for hooks messages
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- cypress/e2e/tests/features/hooks.spec.ts
+++ cypress/e2e/tests/features/hooks.spec.ts
@@ -2,9 +2,11 @@ import HomePagePo from '@rancher/cypress/e2e/po/pages/home.po';
 
 import { SlidingBadgePo } from '@/cypress/e2e/po/hook.po';
 import ChatPo from '@/cypress/e2e/po/chat.po';
+import { HistoryPo } from '../../po/history.po';
 
 describe('Hooks', () => {
   const chat = new ChatPo();
+  const history = new HistoryPo();
 
   beforeEach(() => {
     cy.login();
@@ -31,6 +33,25 @@ describe('Hooks', () => {
 
       message.containsText('Please analyse the Cluster "local" and troubleshoot any problems.');
       message.containsText('See More');
+
+      const response = chat.getMessage(2);
+
+      response.isCompleted();
+
+      history.open();
+
+      const chatItem = history.chatItem(0);
+
+      chatItem.self().contains('Please analyse the Cluster "local" and troubleshoot any problems.');
+
+      chatItem.showTooltip();
+
+      chatItem.tooltip().within(() => {
+        // The tooltip content should be: Summary + Content + Created at information
+        cy.contains('Please analyse the Cluster "local" and troubleshoot any problems.').should('be.visible');
+        cy.contains('Explain what the "active" state means').should('be.visible');
+        cy.contains('Started on').should('be.visible');
+      });
     });
   });
 });
