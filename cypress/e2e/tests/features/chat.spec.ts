5b563f46b6b733551eadfb7d1707d8a06b04ae25
Francesco Torchia <francesco.torchia@suse.com>
2026-02-18 15:38:38 +0100

    Add e2e tests
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- cypress/e2e/tests/features/chat.spec.ts
+++ cypress/e2e/tests/features/chat.spec.ts
@@ -4,7 +4,9 @@ import ClusterManagerListPagePo from '@rancher/cypress/e2e/po/pages/cluster-mana
 import { FleetDashboardListPagePo } from '@rancher/cypress/e2e/po/pages/fleet/fleet-dashboard.po';
 import UsersPo from '@rancher/cypress/e2e/po/pages/users-and-auth/users.po';
 import ExtensionsPagePo from '@rancher/cypress/e2e/po/pages/extensions.po';
-import { SettingsPagePo } from '@rancher/cypress/e2e/po/pages/global-settings/settings.po';
+import ProductNavPo from '@rancher/cypress/e2e/po/side-bars/product-side-nav.po';
+import { SettingsPagePo as GlobalSettings } from '@rancher/cypress/e2e/po/pages/global-settings/settings.po';
+import { SettingsPagePo } from '@/cypress/e2e/po/settings.po';
 
 import ChatPo from '@/cypress/e2e/po/chat.po';
 
@@ -15,7 +17,7 @@ describe('Chat', () => {
     cy.login();
   });
 
-  describe('it should be available in main UI areas', () => {
+  describe('Availability across UI products', () => {
     it('Home', () => {
       HomePagePo.goTo();
 
@@ -73,13 +75,200 @@ describe('Chat', () => {
     });
 
     it('Settings', () => {
-      const settingsPage = new SettingsPagePo('_');
+      const settingsPage = new GlobalSettings('_');
 
       settingsPage.goTo();
       settingsPage.waitForPage();
 
       chat.open();
       chat.close();
+    });
+  });
+
+  describe('Disconnections handling', () => {
+    beforeEach(() => {
+      cy.login();
+    });
+
+    it('it should support reconnections on settings changes', () => {
+      // Go to Settings page
+      const globalSettings = new GlobalSettings('_');
+      const settingsPage = new SettingsPagePo();
+
+      globalSettings.goTo();
+      globalSettings.waitForPage();
+
+      const sideNav = new ProductNavPo();
+
+      sideNav.navToSideMenuEntryByLabel('AI Assistant');
+
+      // Open the chat
+      chat.open();
+
+      const welcomeMessage = chat.getMessage(1);
+
+      welcomeMessage.isCompleted();
+
+      chat.sendMessage('User request');
+
+      const userMessage = chat.getMessage(2);
+
+      userMessage.containsText('User request');
+
+      const resultMessage = chat.getMessage(3);
+
+      resultMessage.isCompleted();
+
+      settingsPage.settings().saveButton().click();
+
+      // Check for the reconnecting phase
+      chat.isNotReady();
+      chat.phase('Reconnecting').should('be.visible');
+
+      chat.isReady(20000);
+      chat.phase('Reconnecting').should('not.exist');
+
+      // Check that the chat is working after reconnection
+      chat.sendMessage('User request after reconnection');
+
+      const userMessageAfterReconnection = chat.getMessage(4);
+
+      userMessageAfterReconnection.containsText('User request after reconnection');
+
+      const resultMessageAfterReconnection = chat.getMessage(5);
+
+      resultMessageAfterReconnection.containsText('Mock service');
+
+      resultMessageAfterReconnection.isCompleted();
+    });
+
+    it('it should support reconnections when opening the chat and settings changes are applying', () => {
+      // Go to Settings page
+      const globalSettings = new GlobalSettings('_');
+      const settingsPage = new SettingsPagePo();
+
+      globalSettings.goTo();
+      globalSettings.waitForPage();
+
+      const sideNav = new ProductNavPo();
+
+      sideNav.navToSideMenuEntryByLabel('AI Assistant');
+
+      // Open the chat
+      chat.open();
+
+      const welcomeMessage = chat.getMessage(1);
+
+      welcomeMessage.isCompleted();
+
+      chat.sendMessage('User request');
+
+      const userMessage = chat.getMessage(2);
+
+      userMessage.containsText('User request');
+
+      const resultMessage = chat.getMessage(3);
+
+      resultMessage.isCompleted();
+
+      settingsPage.settings().saveButton().click();
+
+      // Check for the reconnecting phase
+      chat.isNotReady();
+      chat.phase('Reconnecting').should('be.visible');
+
+      chat.close();
+      chat.open();
+
+      // Reconnection should happen and the chat should be ready after that
+      chat.isNotReady();
+      chat.phase('Connecting').should('be.visible');
+
+      chat.isReady(20000);
+      chat.phase('Connecting').should('not.exist');
+
+      // Check that the chat is working after reconnection
+      chat.sendMessage('User request after reconnection');
+
+      const userMessageAfterReconnection = chat.getMessage(4);
+
+      userMessageAfterReconnection.containsText('User request after reconnection');
+
+      const resultMessageAfterReconnection = chat.getMessage(5);
+
+      resultMessageAfterReconnection.containsText('Mock service');
+
+      resultMessageAfterReconnection.isCompleted();
+    });
+
+    it('it should handle disconnections when the ai service becomes unavailable', () => {
+      HomePagePo.goTo();
+
+      chat.open();
+
+      const welcomeMessage = chat.getMessage(1);
+
+      welcomeMessage.isCompleted();
+
+      cy.installRancherAIService({ waitForAIServiceReady: false });
+
+      // Check for the disconnected phase and error message
+      chat.isNotReady();
+      chat.phase('Disconnected').should('be.visible');
+
+      chat.getErrorMessage(1).containsText('Rancher AI Agent not found. Please ensure the Rancher AI chart is installed.');
+
+      // Check for the reconnecting phase
+      chat.isNotReady();
+      chat.phase('Connecting').should('be.visible');
+
+      chat.isReady(20000);
+      chat.phase('Connecting').should('not.exist');
+
+      // Check that the chat is working after reconnection
+      chat.sendMessage('User request after reconnection');
+
+      const userMessageAfterReconnection = chat.getMessage(2);
+
+      userMessageAfterReconnection.containsText('User request after reconnection');
+
+      const resultMessageAfterReconnection = chat.getMessage(3);
+
+      resultMessageAfterReconnection.containsText('Mock service');
+
+      resultMessageAfterReconnection.isCompleted();
+    });
+
+    it('it should support reconnections when opening the chat and the AI service is unavailable', () => {
+      HomePagePo.goTo();
+
+      cy.installRancherAIService({ waitForAIServiceReady: false });
+
+      chat.open();
+
+      // Check for the reconnecting phase
+      chat.isNotReady();
+      chat.phase('Connecting').should('be.visible');
+
+      chat.isReady(20000);
+      chat.phase('Connecting').should('not.exist');
+
+      // Check that the chat is working after reconnection
+      chat.sendMessage('User request after reconnection');
+
+      const userMessageAfterReconnection = chat.getMessage(2);
+
+      userMessageAfterReconnection.containsText('User request after reconnection');
+
+      const resultMessageAfterReconnection = chat.getMessage(3);
+
+      resultMessageAfterReconnection.containsText('Mock service');
+
+      resultMessageAfterReconnection.isCompleted();
+    });
+
+    after(() => {
+      cy.installRancherAIService();
     });
   });
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,6 +1,7 @@
 import '@rancher/cypress/support/e2e';
 import 'cypress-real-events';
 import './commands/rancher-api';
+import './commands/rancher-ai-service';
 import './commands/llm-mock-service-api';
 import './commands/chat-history';
 import './commands/multi-agent';
