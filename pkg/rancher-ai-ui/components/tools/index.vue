13a54f933845ba403c43e2d1e29f16ed6f902897
Francesco Torchia <francesco.torchia@suse.com>
2026-03-31 09:50:37 +0200

    temp-add-tools-generic-component
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ pkg/rancher-ai-ui/components/tools/index.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import { camelCase, upperFirst } from 'lodash';
+import { type PropType, defineAsyncComponent, onMounted, ref } from 'vue';
+import { useStore } from 'vuex';
+import { useI18n } from '@shell/composables/useI18n';
+import { ToolAction } from '../../types';
+
+const store = useStore();
+const { t } = useI18n(store);
+
+const props = defineProps({
+  label: {
+    type:    String,
+    default: '',
+  },
+  tools: {
+    type:    Array as PropType<ToolAction[]>,
+    default: () => [],
+  },
+});
+
+const toolComponents = ref<Record<string, any>>({});
+
+async function loadToolComponents() {
+  const loaderPromises = props.tools.map(async({ toolName: name }) => {
+    const path = upperFirst(camelCase(name));
+
+    try {
+      await import(`./${ path }.vue`);
+
+      const component = defineAsyncComponent(() => import(`./${ path }.vue`));
+
+      return {
+        name,
+        component
+      };
+    } catch (error) {
+      console.warn(`Tool component not found: ${ path }.vue`, error);
+
+      return { name };
+    }
+  });
+
+  const all = await Promise.all(loaderPromises);
+
+  all.forEach(({ name, component }) => {
+    if (component) {
+      toolComponents.value[name] = component;
+    }
+  });
+}
+
+const emit = defineEmits([]);
+
+onMounted(() => {
+  loadToolComponents();
+});
+</script>
+
+<template>
+  <div class="chat-tools-container">
+    <div class="chat-msg-tools-title">
+      <span>{{ props.label || 'TOOLS' }}</span>
+    </div>
+    <div class="chat-msg-tools-container">
+      <div class="chat-msg-tool-tags">
+        <div
+          v-for="(tool, index) in props.tools"
+          :key="index"
+          class="mt-2 chat-msg-tools"
+        >
+          <component
+            :is="toolComponents[tool.toolName]"
+            :tool="tool"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.chat-msg-tools-title {
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  color: var(--on-secondary);
+
+  span {
+    font: 9px sans-serif;
+    font-weight: 500;
+  }
+}
+
+.chat-msg-tools-container {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.chat-msg-tool-tags {
+  display: flex;
+  gap: 4px;
+  flex-wrap: wrap;
+  margin-top: 4px;
+}
+
+.chat-msg-tools {
+  display: flex;
+  gap: 6px;
+  flex-wrap: wrap;
+  margin-top: 2px;
+}
+
+.chat-msg-tools-more {
+  color: #94a3b8;
+  cursor: pointer;
+}
+</style>
