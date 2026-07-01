0d10488c68b072f218bd2c03f34197a76c6701bf
Francesco Torchia <francesco.torchia@suse.com>
2026-07-01 12:04:07 +0200

    Disable console and context panels when chat is not initialized; add Preparing chat process label
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/rancher-ai-ui/components/panels/Messages.vue
+++ pkg/rancher-ai-ui/components/panels/Messages.vue
@@ -229,11 +229,12 @@ onBeforeUnmount(() => {
       :disabled="false"
     />
     <Processing
-      v-if="!props.disabled"
+      v-if="!props.activeChatId || !props.disabled"
+      data-test-prefix="message"
       class="chat-message-processing-label text-label"
       :class="{
         /* It avoids pushing the System messages up (Welcome template) */
-        'sticky-bottom': formattedMessages.filter((m: Message) => m.role === Role.User).length > 0
+        'sticky-bottom': !props.activeChatId || formattedMessages.filter((m: Message) => m.role === Role.User).length > 0
       }"
       :phase="props.processingState?.phase"
       :label="props.processingState?.label"
